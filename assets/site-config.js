940f49968c00db5a60024ceb60fda964fcc17819
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2026-05-27 20:34:34 +0100

    update site to show pwa

--- assets/site-config.js
+++ assets/site-config.js
@@ -1,4 +1,5 @@
 window.SPFC_SITE_CONFIG = {
+  pwaUrl: "https://spfcapp.solasislimited.org",
   googlePlayUrl: "https://play.google.com/store/apps/details?id=org.solasislimited.spfc",
   androidFullApkUrl:
     "https://github.com/Solasis-Limited/SPFC-Website/releases/download/v1.1.0/application-cb94377c-d5eb-4a90-b49c-06f2658d4e9e.apk",
